a1ea1cfa450b9a47bab07c405fe7a464f96eea75
Jayanta Sarkar <jsarkar14683@gmail.com>
2026-04-23 15:26:14 +0530

    commit

---
+++ layers/UI/components/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Layer Base Button</p>
+  <button class="btn">
+    <slot />
+  </button>
+  </div>
+</template>
+
+<style scoped>
+.btn {
+  padding: 8px 16px;
+  background: black;
+  color: white;
+  border-radius: 6px;
+}
+</style>
---
+++ layers/UI/nuxt.config.ts
@@ -0,0 +1,7 @@
+
+import { defineNuxtConfig } from 'nuxt/config';
+export default defineNuxtConfig({
+  // optional defaults for all apps using this layer
+});
+
+
