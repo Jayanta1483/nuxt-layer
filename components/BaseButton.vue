25f0a136c41ebea74be73b55d9efa4f109e74ea8
Jayanta Sarkar <jsarkar14683@gmail.com>
2026-06-24 12:24:17 +0530

    Nuxt UI added and button changed

--- components/BaseButton.vue
+++ components/BaseButton.vue
@@ -1,17 +1,9 @@
 <template>
   <div>
-    <p>Layer Base Button</p>
-  <button class="btn">
-    <slot />
-  </button>
+   <UButton >Base Button</UButton>
   </div>
 </template>
 
 <style scoped>
-.btn {
-  padding: 8px 16px;
-  background: black;
-  color: white;
-  border-radius: 6px;
-}
+
 </style>
